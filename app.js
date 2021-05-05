48431d5cb728f62a2abaccc726202dd1b3cc33f7
Mani <mani979084@gmail.com>
2021-05-05 12:37:12 +0530

    Add cool face API

--- app.js
+++ app.js
@@ -164,7 +164,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('./campground/error', { err });
 })
 
-app.listen('3000', () => {
-    console.log('listening on 3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
 })
 
